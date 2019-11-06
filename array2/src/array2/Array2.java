a9867797b98720ac0ecfcbd20353747e28e66062
bh1990 <56873720+bh1990@users.noreply.github.com>
2019-11-06 21:33:50 +0700

    Add files via upload

---
+++ array2/src/array2/Array2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array2;
+
+/**
+ *
+ * @author user
+ */
+public class Array2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] keluarga= {"Bagus hartanto", "Ulfatul zannah", "M.Aryasatya",};
+    System.out.println("kepala keluarga:"+keluarga[0]);
+    System.out.println("isteri:"+keluarga[1]);
+    System.out.println("anak:"+keluarga[2]);
+    
+  }
+
+}
+    
